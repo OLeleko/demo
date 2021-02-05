158ae75bcaf25854dba7e904df76e37e91fefc3d
Oleksandr Leleko <o.leleko@smiddle.lab>
2021-02-05 12:26:55 +0200

    N+1 problem fix

--- src/main/java/com/example/demo/service/MealService.java
+++ src/main/java/com/example/demo/service/MealService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Meal;
 import com.example.demo.model.Menu;
 import com.example.demo.repository.MealRepository;
 import com.example.demo.repository.MenuRepository;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
@@ -21,6 +22,7 @@ public class MealService {
     }
 
     @Transactional
+    @CacheEvict(value = "findByDate", allEntries = true)
     public Meal create(Meal meal, int menu_id) {
         if (!meal.isNew()) {
             return null;
@@ -30,17 +32,20 @@ public class MealService {
         return mealRepository.save(meal);
     }
 
-    @Transactional
+
     public Meal findById(int id) {
         return mealRepository.findById(id)
                 .orElse(null);
     }
 
+    @Transactional
+    @CacheEvict(value = "findByDate", allEntries = true)
     public void delete(int id) {
         checkNotFoundWithId(mealRepository.delete(id), id);
     }
 
     @Transactional
+    @CacheEvict(value = "findByDate", allEntries = true)
     public void update(Meal meal) {
         Assert.notNull(meal, "meal must not be null");
         mealRepository.save(meal);
